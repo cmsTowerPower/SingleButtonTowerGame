20ab72798d5d60d79165008638c98a980296922d
MartinHaring <martin.haring1106@gmail.com>
2019-03-13 17:17:56 +0100

    Implement basic button behavior

---
+++ Assets/Scripts/Init.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// MH - Init to win it
+public class Init : MonoBehaviour
+{
+    void Awake() {
+        setPlayerPrefs();
+        SceneManager.LoadScene("Game");
+    }
+
+    void setPlayerPrefs() {
+
+        /* 
+            The input state indicates the current state of the random object lifecycle
+            0 = About to spawn
+            1 = Growing
+            2 = Moving
+            3 = Dropping
+        */ 
+        PlayerPrefs.SetInt("inputState", 0);
+    }
+}
---
+++ Assets/Scripts/ObjectGrower.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// MH - the second button press should stop an ongoing growth
+public class ObjectGrower : MonoBehaviour
+{
+    public float growthFactor = 0.05f;
+    public float minSize = 0.5f;
+    public float maxSize = 5.5f;
+    private bool isGrowing = false;
+
+    // in this state, the object grows all the time - a button press will stop the growth
+    void Update() {
+        if (isGrowing) {
+            float x = this.gameObject.transform.localScale.x + growthFactor;
+            if (x > maxSize || x < minSize) growthFactor *= -1;
+
+            grow();
+
+            if (Input.GetKeyUp(KeyCode.Space)) greatify();
+        }
+        if (PlayerPrefs.GetInt("inputState") == 1) isGrowing = true;
+    }
+
+    void grow() => this.gameObject.transform.localScale += new Vector3(growthFactor, growthFactor);
+    void greatify() {
+        PlayerPrefs.SetInt("inputState", 2);
+        Destroy(this);
+    }
+}
